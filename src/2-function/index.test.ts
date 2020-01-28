44247631b16f39fb952ca1400782b62bd14a4e5c
Alberto Basalo <albertobasalo@hotmail.com>
2020-01-28 16:37:48 +0100

    feat: 2-function tests

--- src/2-function/index.test.ts
+++ src/2-function/index.test.ts
@@ -1,18 +1,23 @@
 /* eslint-disable max-nested-callbacks */
-import { writeOneSentencePerLine } from './index';
+import {
+  getAbstractionLevels,
+  getLimitCyclomaticComplexity,
+  getNestedBlocks,
+  writeOneSentencePerLine,
+} from './index';
 
 describe('Structure your code in blocks', () => {
   test('One simple sentence per line', () => {
-    expect(writeOneSentencePerLine()).toEqual(undefined);
+    expect(writeOneSentencePerLine()).toEqual(true);
   });
   test('Avoid nested blocks', () => {
-    expect(true).toEqual(true);
+    expect(getNestedBlocks()).toEqual(2);
   });
   test('Limit the cyclomatic complexity', () => {
-    expect(true).toEqual(true);
+    expect(getLimitCyclomaticComplexity()).toEqual(8);
   });
   test('One abstraction level per function', () => {
-    expect(true).toEqual(true);
+    expect(getAbstractionLevels()).toEqual(1);
   });
   test('Limit your functions length', () => {
     expect(true).toEqual(true);
--- src/2-function/index.ts
+++ src/2-function/index.ts
@@ -4,7 +4,7 @@ export function writeSmallFunctions(priciples: string[]): void {
   priciples.forEach(printToConsole);
 }
 
-export function writeOneSentencePerLine(): void {
+export function writeOneSentencePerLine(): boolean {
   const year = '2020';
   const category = 'Código limpio';
   const title = 'Código artesano: limpia tu código con la artesanía del Software.';
@@ -14,9 +14,30 @@ export function writeOneSentencePerLine(): void {
     slug = getSlug(slug);
   }
   printToConsole(slug);
+  return true;
 }
 
-// https://gist.github.com/codeguy/6684588
+export function getNestedBlocks(): number {
+  const MAX_DEPTH = 2;
+  return MAX_DEPTH;
+}
+
+export function getLimitCyclomaticComplexity(): number {
+  const COMPLEXITY = 8;
+  return COMPLEXITY;
+}
+
+export function getAbstractionLevels(): number {
+  const LEVELS_OF_ABSTRACTION = 1;
+  return LEVELS_OF_ABSTRACTION;
+}
+
+export function getMaximumFunctionsLenght(): number {
+  const MAX_LINES_PER_FUNCTION = 20;
+  return MAX_LINES_PER_FUNCTION;
+}
+
+// ORIGINAL: https://gist.github.com/codeguy/6684588
 
 function getSlug(slug: string): string {
   slug = slug.replace(/^\s+|\s+$/g, '');
@@ -32,7 +53,9 @@ function replaceNonCommonChars(slug: string): string {
   const START = 0;
   const END = from.length;
   for (let i = START; i < END; i++) {
-    slug = slug.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    const nonCommonCharacter = new RegExp(from.charAt(i), 'g');
+    const similarCommonCharacter = to.charAt(i);
+    slug = slug.replace(nonCommonCharacter, similarCommonCharacter);
   }
   return slug;
 }
